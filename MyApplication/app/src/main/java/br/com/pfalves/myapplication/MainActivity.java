ed433e628f6e257374642208f5099dae69134d49
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-01-29 11:44:06 -0300

    Greg

--- MyApplication/app/src/main/java/br/com/pfalves/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/br/com/pfalves/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.pfalves.myapplication;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,10 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button button = (Button) findViewById(R.id.btnInicial);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SegundaActividy.class));
+            }
+        });
     }
 
-    public void abrirSegundaAcitivity(View view){
-        startActivity(new Intent(getBaseContext(),SegundaActividy.class));
-    }
 }
