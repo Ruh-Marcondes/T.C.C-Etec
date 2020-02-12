ac6d81f1f7a1700dde4bc502f5526a4bf282eb90
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-12 20:28:13 -0300

    Aula 1 <#>

--- Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
+++ Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
@@ -3,9 +3,12 @@ package br.com.pfalves.gregory_bebe;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+// A classe extende da AppCompactActivity
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    // Ele executa o 'onCreate' da classe "Paid"
+    // Depois executa o setContenteView
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
