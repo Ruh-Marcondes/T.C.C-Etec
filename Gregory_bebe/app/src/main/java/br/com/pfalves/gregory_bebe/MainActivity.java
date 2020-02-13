8789fba2254e413819f3ba8ffa12136ab809cc05
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-13 20:57:11 -0300

    botão criado e click para ver

--- Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
+++ Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.pfalves.gregory_bebe;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,9 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    //Metodo para abrir segunda activity na tela
-    public void abrirSegundaAcitivity(View view){
-        startActivity(new Intent(getBaseContext(),SegundaActivity.class));
+
+        //segunda forma
+        Button button = (Button) findViewById(R.id.btninit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SegundaActivity.class));
+            }
+        });
+
     }
 }
