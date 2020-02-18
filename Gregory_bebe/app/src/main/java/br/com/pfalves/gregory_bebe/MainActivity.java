e83164c0ddd5f3c365571833ea9a3b0cec34fb7c
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-17 21:38:27 -0300

    ALGUMAS MODIFICAÇÕEA

--- Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
+++ Gregory_bebe/app/src/main/java/br/com/pfalves/gregory_bebe/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.btninit);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 startActivity(new Intent(getBaseContext(),SegundaActivity.class));
             }
         });
