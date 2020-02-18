463d9876e5e6ea4be678171cafb86686c02afe64
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-17 22:28:22 -0300

    btn feito

--- Greg/app/src/main/java/br/com/hack/MainActivity.java
+++ Greg/app/src/main/java/br/com/hack/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.hack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Definindo q quando clikar no btn ele ira abrir a segunda pagina
+
+        Button btn = (Button) findViewById(R.id.btninit);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),SegundaAcitivity.class));
+            }
+        });
     }
 }
