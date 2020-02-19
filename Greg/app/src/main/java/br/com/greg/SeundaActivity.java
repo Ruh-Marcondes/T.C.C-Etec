d977eb28cf73101b163db843f45bc8e0b96d8aac
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-19 18:52:23 -0300

    Base do meu filinhooo

---
+++ Greg/app/src/main/java/br/com/greg/SeundaActivity.java
@@ -0,0 +1,15 @@
+package br.com.greg;
+
+import android.os.Bundle;
+
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SegundaAcitivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
