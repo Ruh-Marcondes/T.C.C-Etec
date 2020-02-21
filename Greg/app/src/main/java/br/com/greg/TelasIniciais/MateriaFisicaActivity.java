6246d5ef5bce4a6a791e7784966d5c11a1a8257b
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-21 08:06:11 -0300

    Tela de Fisica criada

--- Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaFisicaActivity.java
+++ Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaFisicaActivity.java
@@ -1,7 +1,18 @@
 package br.com.greg.TelasIniciais;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.greg.R;
 
 public class MateriaFisicaActivity extends AppCompatActivity {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materias_fisica);
+    }
 }
