dbf2bdb52aaa11fd68ba7744069da752a1c26b48
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-21 10:43:51 -0300

    Assim os botões estão meio fora e lugar
    ¬¬¬ Pesquisar sobre o layout
    ¬¬¬ Fusar mais para descubrir as funçoes

--- Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaFisicaActivity.java
+++ Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaFisicaActivity.java
@@ -1,17 +1,13 @@
 package br.com.greg.TelasIniciais;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import br.com.greg.R;
 
 public class MateriaFisicaActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materias_fisica);
     }
