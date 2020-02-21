db9cb56b16f0ea4df8dcd7f0a010bf4f757ade06
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-02-21 07:56:02 -0300

    Buttons não Prontos mostram ms

--- Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaMatematicaActivity.java
+++ Greg/app/src/main/java/br/com/greg/TelasIniciais/MateriaMatematicaActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,10 +12,11 @@ import br.com.greg.Materias.Afim.AfimAcitivity;
 import br.com.greg.R;
 
 public class MateriaMatematicaActivity extends AppCompatActivity {
+    public  static  String m = "Ñ Terminado";
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_materias);
+        setContentView(R.layout.activity_materias_math);
 
         Button btn = (Button) findViewById(R.id.btn1);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +25,36 @@ public class MateriaMatematicaActivity extends AppCompatActivity {
                 startActivity(new Intent(getBaseContext(),AfimAcitivity.class));
             }
         });
+
+       btn = (Button) findViewById(R.id.btn2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn = (Button) findViewById(R.id.btn3);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn = (Button) findViewById(R.id.btn4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
+            }
+        });
+        btn = (Button) findViewById(R.id.btn5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),m,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
