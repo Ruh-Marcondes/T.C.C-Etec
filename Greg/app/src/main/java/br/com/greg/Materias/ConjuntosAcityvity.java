a39dee6874cbe5918faf5b8bbf4dbf4b8ec0552e
Ruh  Marcondes <marcondes.ruahma@gmail.com>
2020-03-03 20:17:10 -0300

    Conjuntos natural

--- Greg/app/src/main/java/br/com/greg/Materias/ConjuntosAcityvity.java
+++ Greg/app/src/main/java/br/com/greg/Materias/ConjuntosAcityvity.java
@@ -1,8 +1,6 @@
 package br.com.greg.Materias;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
