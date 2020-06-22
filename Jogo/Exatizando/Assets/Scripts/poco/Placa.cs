265dfd28ca5296d6a8e53f8511004b2fe49c9133
Ruh-Marcondes <47475949+Ruh-Marcondes@users.noreply.github.com>
2020-06-22 08:34:58 -0300

    Arrumando Project

---
+++ Jogo/Exatizando/Assets/Scripts/poco/Placa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placa : MonoBehaviour
+{
+
+    private string[] avisos  = new string[]{"Bem Vindo a Exatizando","Você deve cumprir os desafios desse mundo para retornar ao seu"}; 
+    public int ID;
+    void Start()
+    {
+        
+
+    }
+    void Update()
+    {
+        
+    }
+    
+     void OnCollisionEnter2D(Collision2D collision){ // quando o objeto toca em algo 
+        if(collision.gameObject.tag == "Player"){
+            switch(ID){
+                case 1:
+                Debug.Log(avisos[0]);
+                break;
+                case 2:
+                Debug.Log("Este é o primeiro desafio");
+                break;
+            }
+            
+        }
+    }
+     void OnCollisionExit2D(Collision2D collision){
+         Debug.Log("You are a baka des");
+
+    }
+}
