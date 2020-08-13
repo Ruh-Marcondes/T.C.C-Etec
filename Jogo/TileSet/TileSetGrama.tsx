e7f611c2df140761e81c506e0e8505606c259c2f
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2020-08-13 16:00:28 -0300

    changeTiles

--- Jogo/TileSet/TileSetGrama.tsx
+++ Jogo/TileSet/TileSetGrama.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="TileSetGrama" tilewidth="32" tileheight="32" tilecount="20" columns="4">
- <image source="../../Imgs/Chão/TileGrama.png" width="152" height="163"/>
+<tileset version="1.4" tiledversion="1.4.2" name="Grama" tilewidth="16" tileheight="16" tilecount="20" columns="5">
+ <image source="../../Imgs/Chão/Tilesetgrama.png" width="80" height="64"/>
 </tileset>
