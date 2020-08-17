98162d589ccc400f15b2da50d6c092dd5a1890b1
Ruh <47475949+Ruh-Marcondes@users.noreply.github.com>
2020-08-17 09:53:12 -0300

    Masmorra

--- Jogo/TileSet/TileSetGrama.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Grama" tilewidth="16" tileheight="16" tilecount="20" columns="5">
- <image source="../../../../UnityJogos/Exato_Teste/Assets/Art/Tile/Tilesetgrama.png" width="80" height="64"/>
-</tileset>
